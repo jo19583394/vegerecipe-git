352abd6e3a7243d9b8cb46d666bf5ad0c78813a8
masayukikamijo <kmjooh8649@gmail.com>
2021-02-13 06:01:23 +0000

    complete favorite_button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,17 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
     if @recipe.user_id != current_user.id
-      @favorite = Favorite.create(user_id: current_user.id, recipe_id: @recipe.id)
+      @favorite = Favorite.create(user_id: current_user.id, recipe_id: params[:recipe_id])
+      redirect_to @recipe
     end
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
-    @favorite = Favorite.find_by(user_id: current_user.id, recipe_id: @recipe.id)
+    @recipe = Recipe.find(params[:recipe_id])
+    @favorite = Favorite.find_by(user_id: current_user.id, recipe_id: params[:recipe_id])
     @favorite.destroy
+    redirect_to @recipe
   end
 end
